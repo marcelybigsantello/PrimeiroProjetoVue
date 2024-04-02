320cd5ceaacf296ec1d6977f933b495c5997e90f
Marcely Santello <marcely@sbsoftware.com.br>
2024-04-01 22:42:34 -0300

    Criação do componente Input que será usado em form

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+
+
+    <div>
+        <input type="text"
+        class="form-control"
+        :name="name"
+        :type="type"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue')">
+    </div>
+    <ul v-if="errors.length" class="errors-list">
+        <li v-for="error in errors">{{ error }}</li>
+    </ul>
+</template>
+<style></style>
+
